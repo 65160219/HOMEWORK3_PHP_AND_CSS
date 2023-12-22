ce4b35ada17eaebe1ef0d3f60aead8c8e1ab7415
65160219 <152172067+65160219@users.noreply.github.com>
2023-12-22 23:29:36 +0700

    Update code

--- MultiplicationTable.php
+++ MultiplicationTable.php
@@ -7,22 +7,34 @@
 <body>
     <?php $multi_x = 2; ?>
     <h1>ตารางสูตรคูณแม่ <?php echo $multi_x ?></h1>
+    </p>
 
     <table>
         <tbody>
             <tr>
-                <td></td>
-                <td>x</td>
-                <td></td>
-                <td>=</td>
-                <td></td>
+                <div>
+                <?php for($i=1 ; $i<=24 ; $i++){ ?>
+                    <?php $sum = $multi_x * $i ; 
+                    $output = $multi_x . " x " . $i ." = " . $sum ;
+                    print $output ; 
+                    echo "<br/>" ; ?>
+                <?php } ?>
+                </div>
             </tr>
         </tbody>
     </table>
+
     <style>
         table {
             width: 150px;
             height: 265px;
+        }div {
+            width: 150px;
+            height: 225px;
+            border: 1px solid black;
+            overflow: auto;
+        }h1 {
+          font-family: 'cursive';
         }
     </style>
 </body>
